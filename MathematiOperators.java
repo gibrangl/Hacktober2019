21639172df9a92a2e51c7c9913ec7fffab1b85ea
ulquiorraon <57140442+ulquiorraon@users.noreply.github.com>
2019-10-29 21:19:34 +0800

    Create MathematiOperators.java

---
+++ MathematiOperators.java
@@ -0,0 +1,13 @@
+Public class Aritmatika {
+    public static void main(String[] args) {
+        System.out.println("1 + 2 =" + (1+2));
+        System.out.println("3 * 2 =" + (3*2));
+        System.out.println("7 / 3 =" + (7/3));
+        System.out.println("3 - 2 =" + (3-2));
+        System.out.println("13 % 5 =" + (13%5));
+        System.out.println("");
+        System.out.println("1 + 2 * 3 =" + (1+2*3));
+        System.out.println("1 + 2 / 3 =" + (1+2/3));
+        System.out.println("9 - 2 % 2 =" + (9-2%2));
+
+    }}
